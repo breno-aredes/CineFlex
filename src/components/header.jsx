9960d8d3b06b205fc140e0ab9fe2248679f76572
breno-aredes <breno_mg10@hotmail.com>
2022-12-09 16:50:04 -0300

    v 0.3.0 bonus botão de voltar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,21 @@
+import { useLocation, useNavigate } from "react-router";
 import styled from "styled-components";
 
 export default function Header() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  function backPage() {
+    navigate(-1);
+  }
+
   return (
     <HeaderContainer>
+      {location.pathname != "/" && (
+        <BackButton onClick={() => backPage()} data-test="go-home-header-btn">
+          <ion-icon name="arrow-back-outline"></ion-icon>
+        </BackButton>
+      )}
       <h1>CINEFLEX</h1>
     </HeaderContainer>
   );
@@ -24,4 +37,19 @@ const HeaderContainer = styled.div`
   position: fixed;
   left: 0px;
   top: 0px;
+`;
+
+const BackButton = styled.button`
+  position: absolute;
+  left: 4px;
+  top: 4px;
+  font-size: 34px;
+  height: 55px;
+  width: 55px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #c3cfd9;
+  color: #e8833a;
+  border: none;
 `;
